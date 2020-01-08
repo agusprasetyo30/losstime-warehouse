d531c5f1cfaf0ffb5400ebe8f6ccbda58855c16e
Agus Prasetyo <agusprasetyo1889@gmail.com>
2020-01-08 14:23:35 +0700

    menambahkan tampilan losstime harian

--- data/admin/losstime.php
+++ data/admin/losstime.php
@@ -15,6 +15,9 @@
    include_once "./template/header.php"
 ?>
 
+<link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+
+
 
 <?php
    if ($_GET['type'] == 'harian') {
@@ -26,3 +29,21 @@
 ?>
 
 <?php include_once "./template/footer.php" ?>
+
+<!-- DataTables -->
+<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+
+<script>
+   $(function () {
+      // $("#example1").DataTable();
+      $('#example1').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": false,
+      "autoWidth": false,
+      });
+   });
+</script>
--- data/admin/losstime/harian.php
+++ data/admin/losstime/harian.php
@@ -1,3 +1,76 @@
+<div class="container-fluid">
+   <div class="row">
+      <div class="col-md-6">
+         <div>Jumlah menit : </div>
+         <div class="jumlah-losstime-harian">
+            <?= 10 ?> Menit
+         </div>
+      </div>
+      <div class="col-md-6 text-right">
+         <div>Periode :</div> 
+         <div class="periode-losstime-harian">
+            <?= date("d/M/Y") ?>
+         </div>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md-12 col-sm-12">
+         <table id="example1" class="table table-bordered table-striped table-hover text-center">
+            <thead>
+               <tr>
+                  <th style="width: 30px">#</th>
+                  <th>Nomer Line</th>
+                  <th>Shift</th>
+                  <th style="width: 500px">Masalah</th>
+                  <th>Jumlah Menit</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td>1. </td>
+                  <td>A42</td>
+                  <td>Pagi</td>
+                  <td>Supplay Terlambat Datang</td>
+                  <td>3</td>
+               </tr>
+               <tr>
+                  <td>2. </td>
+                  <td>A42</td>
+                  <td>Malam</td>
+                  <td>Terlambat Datang</td>
+                  <td>10</td>
+               </tr>
+               <tr>
+                  <td>3. </td>
+                  <td>A42</td>
+                  <td>Pagi</td>
+                  <td>Terlambat Datang</td>
+                  <td>10</td>
+               </tr>
+               <tr>
+                  <td>4. </td>
+                  <td>A42</td>
+                  <td>Pagi</td>
+                  <td>Terlambat Datang</td>
+                  <td>10</td>
+               </tr>
+               <tr>
+                  <td>5. </td>
+                  <td>A42</td>
+                  <td>Malam</td>
+                  <td>Terlambat Datang</td>
+                  <td>10</td>
+               </tr>
+               <tr>
+                  <td>6. </td>
+                  <td>A42</td>
+                  <td>Pagi</td>
+                  <td>Terlambat Datang</td>
+                  <td>10</td>
+               </tr>
+            </tbody>      
+         </table>
+      </div>
+   </div>
+</div>
 
-
-<h2>Harian</h2>
