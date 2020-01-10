09defb522b16652c9b9744a317f92141bcd6f599
Agus Prasetyo <agusprasetyo1889@gmail.com>
2020-01-10 13:30:22 +0700

    update pengguna

--- data/admin/pengguna.php
+++ data/admin/pengguna.php
@@ -10,6 +10,25 @@
 
 ?>
 
-<h2>Ini tampilan untuk pengguna</h2>
+<hr>
+
+<div class="container-fluid">
+   <div class="row justify-content-center">
+      <div class="col-md-6">
+         <div class="card card-default">
+            <div class="card-header">
+               <h3 class="card-title">
+                  <i class="fa fa-user" aria-hidden="true"></i>
+                  Pengguna
+               </h3>
+            </div>
+            <div class="card-body">
+               <label for="">Nama</label>
+               <input type="text" class="form-control" name="" id="">
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
 
 <?php include_once "./template/footer.php" ?>
